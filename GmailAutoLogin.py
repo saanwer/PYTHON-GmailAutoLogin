a56bf7081eaa5b69ab01ea976649e75ce72b87c1
WarLord <saanwer@users.noreply.github.com>
2017-03-21 15:11:53 +0530

    Create GmailAutoLogin.py

---
+++ GmailAutoLogin.py
@@ -0,0 +1,36 @@
+import argparse, os, time
+import urlparse, random
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#Facebook Auto Login
+#Coded by | WarLord
+#https://github.com/saanwer
+def getID(url):
+	pUrl = urlparse.urlparse(url)
+	return urlparse.parse_qs(pUrl.query)['id'][0]
+
+def Main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("email", help="Gmail email")
+	parser.add_argument("password", help="Gmail password")
+	args = parser.parse_args()
+
+	browser = webdriver.Firefox()
+	browser.get("https://accounts.google.com")
+
+	emailElement = browser.find_element_by_id("Email")
+	emailElement.send_keys(args.email)
+	emailElement.submit()
+	time.sleep(4)
+	passElement = browser.find_element_by_id("Passwd")
+	passElement.send_keys(args.password)
+	passElement.submit()
+	time.sleep(5)
+	browser.get("https://mail.google.com/mail/u/0/")
+
+	os.system('cls')#os.system('clear') if Linux
+	print "[+] Auto login success!"
+	browser.close()
+
+if __name__ == "__main__":
+	Main()
